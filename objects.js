507770a7f436a9f956d53b84b6e0ea4e3d6a6755
RomanRiesen <roman.riesen42@gmail.com>
2018-08-05 08:56:49 +0000

    Touch support still not working, but added destructable springs

--- objects.js
+++ objects.js
@@ -1,20 +1,24 @@
 var World = function (ctx, canv, min, max, dT) {
     this.ctx = ctx;
     this.canv = canv;
-    this.min = min || new Vector(0,0);
-    this.max = max || new Vector(0,0);
+    this.min = min || new Vector(-100,-100);//TODO introduce world coordinates everywhere
+    this.max = max || new Vector(100,100);
     this.dT = dT || 1;
     this.t = 0;
-    this.drag = 0.001;//around 0.001 is reasonable, higher when interactive
+    this.drag = 0.002;//around 0.001 is reasonable, higher when interactive
     this.objects = [];//list of all objects, updated in world.update
-    this.forceField = ()=>{return new Vector(0,0);}//function(pos){return new Vector(0,0.1);};
+    this.forceField = function(pos){return new Vector(0,0.05);};//()=>{return new Vector(0,0);}
     this.mouseForceFactor = 0.1;
+    this.canvRect = this.canv.getBoundingClientRect();//FIXME Must be updated on windowsize change
 
     this.update = () => {
-        ctx.clearRect(0,0,this.canv.width,this.canv.height)
+        ctx.clearRect(0,0,this.canv.width,this.canv.height);
         this.t += this.dT;
         this.objects.map(o => o.update(this));
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 2;
         this.objects.map(o => o.draw(this));
+        this.ctx.stroke();
     }
 
     this.addObject = obj => {
@@ -23,26 +27,37 @@ var World = function (ctx, canv, min, max, dT) {
         return obj
     }
 
-    this.normalizeMousePosition = e => {
-        var rect = this.canv.getBoundingClientRect();
-        canvMousePos = new Vector();
-        X = e.touches ? e.touches[0].clientX : e.clientX
-        Y = e.touches ? e.touches[0].clientY : e.clientY
-        canvMousePos.x = X - rect.left;
-        canvMousePos.y = Y - rect.top;
-        return canvMousePos;
+    this.toWorldVector = vec => {
+        var ret = new Vector();
+        ret.x = (vec.x / this.canvRect.width)*Math.abs(this.max.x - this.min.x)+this.min.x;
+        ret.y = (vec.y / this.canvRect.height)*Math.abs(this.max.y - this.min.y)+this.min.x;
+        return ret;
+    }
+
+    this.toCanvasCoordinates = vec => {
+        var ret = new Vector();
+        ret.x = ((vec.x-this.min.x) / Math.abs(this.max.x - this.min.x)) * this.canvRect.width;
+        ret.y = ((vec.y-this.min.x) / Math.abs(this.max.y - this.min.y)) * this.canvRect.height;
+        return ret;
+    }
+
+    this.normalizeMousePosition = (e, world) => {
+        return new Vector(e.offsetX, e.offsetY)
     }
 
     canv.onmousedown = e => {
-        this.objects.map(o => {if(o.mousedown) o.mousedown(this.normalizeMousePosition(e))})
+        var v = this.normalizeMousePosition(e, world)
+        this.objects.map(o => {if(o.mousedown) o.mousedown(v)})
     };
 
     canv.onmousemove = e => {
-        this.objects.map(o => {if(o.mousemove) o.mousemove(this.normalizeMousePosition(e))})
+        var v = this.normalizeMousePosition(e, world);
+        this.objects.map(o => {if(o.mousemove) o.mousemove(v)})
     };
 
     canv.onmouseup = e => {
-        this.objects.map(o => {if(o.mouseup) o.mouseup(this.normalizeMousePosition(e))})
+        var v = this.normalizeMousePosition(e, world)
+        this.objects.map(o => {if(o.mouseup) o.mouseup(v)})
     };
 
     canv.ontouchstart = e => {e.preventDefault(); canv.onmousedown(e);}
@@ -74,9 +89,8 @@ var Dot = function (pos, weight, fixed) {
     }
 
     this.draw = world => {
-        //ctx.beginPath();
+        //ctx.moveTo(this.pos.x, this.pos.y);
         //ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
-        //ctx.stroke();
     }
 
     this.mousedown = V => {
@@ -100,24 +114,27 @@ var Dot = function (pos, weight, fixed) {
 var Spring = function (p1, p2, length) {
     this.p1 = p1 || new Dot(Vector(0,0));
     this.p2 = p2 || new Dot(Vector(0,1));
-    this.distance = length || p1.pos.subtract(p2.pos).getLength();
-    this.coefficient = 1;
+    this.distance = length || this.p1.pos.subtract(this.p2.pos).getLength();
+    this.coefficient = 1.0;
+    this.maxForce = 20;
+    this.torn = false;
 
     this.update = world => {
+        if(this.torn) return;
         var dir = this.p2.pos.subtract(this.p1.pos);
         var gap = dir.getLength();
         dir.divideBy(gap);//normalize dir
         var diff = gap-this.distance
         var force = Math.abs(diff)*this.coefficient
         dir.multiplyBy(force*Math.sign(diff))
-        p1.force.addTo(dir);
-        p2.force.addTo(dir.multiply(-1));
+        this.p1.force.addTo(dir);
+        this.p2.force.addTo(dir.multiply(-1));
+        if(dir.getLength() > this.maxForce) this.torn = true;
     };
 
     this.draw = world => {
-        ctx.beginPath();
-        ctx.moveTo(p1.pos.x, p1.pos.y);
-        ctx.lineTo(p2.pos.x, p2.pos.y);
-        ctx.stroke();
+        if(this.torn) return;
+        ctx.moveTo(this.p1.pos.x, this.p1.pos.y);
+        ctx.lineTo(this.p2.pos.x, this.p2.pos.y);
     };
 }
